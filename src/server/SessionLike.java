e5804fdb97cc10a56874b27141949b4af96c9d6b
D054525 <Iasd01S10bS>
2019-06-12 11:32:43 +0200

    SessionLike as runnable

--- src/server/SessionLike.java
+++ src/server/SessionLike.java
@@ -3,7 +3,7 @@ package server;
 import java.util.UUID;
 import game.User;
 
-public interface SessionLike {
+public interface SessionLike extends Runnable{
 	public UUID getUUID();
 	public User getUser();
 }
